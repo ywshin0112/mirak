d8b5c0332d4ccda098983cdfa83f58719018dc2b
Lanjangju <acj119@naver.com>
2023-03-06 12:36:03 +0900

    조인 수정

--- mirak/src/main/java/kr/co/mirak/controller/LoginController.java
+++ mirak/src/main/java/kr/co/mirak/controller/LoginController.java
@@ -3,13 +3,12 @@ package kr.co.mirak.controller;
 import javax.servlet.http.HttpSession;
 
 import org.mybatis.spring.SqlSessionTemplate;
-<<<<<<< HEAD
+
 
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-=======
->>>>>>> branch 'Member' of https://github.com/ywshin0112/mirak.git
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.co.mirak.HomeController;
 import kr.co.mirak.member.MemberVO;
 
 
@@ -26,36 +26,18 @@ public class LoginController {
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
 	
-<<<<<<< HEAD
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 
-
-	@RequestMapping(value = "/member", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-=======
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginView(@ModelAttribute("Member") MemberVO vo) {
 		System.out.println("로그인 화면으로 이동...");
 		return "login";
->>>>>>> branch 'Member' of https://github.com/ywshin0112/mirak.git
 	}
-<<<<<<< HEAD
 	
-=======
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(MemberVO vo, HttpSession session) {
@@ -70,6 +52,5 @@ public class LoginController {
 //			return "login";
 		return "login";
 	}
->>>>>>> branch 'Member' of https://github.com/ywshin0112/mirak.git
 }
 
--- mirak/src/main/java/kr/co/mirak/member/LoginService.java
+++ mirak/src/main/java/kr/co/mirak/member/LoginService.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 package kr.co.mirak.member;
 
@@ -7,20 +5,13 @@ import java.util.List;
 
 public interface LoginService {
 	List<MemberVO> list();
-	MemberVO info(int id);
-	int insert(MemberVO vo);
-	int update(MemberVO vo);
-	int delete(int id);
-=======
-package kr.co.mirak.member;
 
-import java.util.List;
-
-public interface LoginService {
-	List<MemberVO> list();
 	MemberVO info(int id);
+
 	int insert(MemberVO vo);
+
 	int update(MemberVO vo);
+
 	int delete(int id);
->>>>>>> branch 'Member' of https://github.com/ywshin0112/mirak.git
+
 }
