3ad61490b1f63c5da926487dacf3bb067ec9b3ba
hi <hi@DESKTOP-6RL4QDK>
2023-03-06 11:05:21 +0900

    왜않되

--- mirak/src/main/java/kr/co/mirak/controller/ClientJoinController.java
+++ mirak/src/main/java/kr/co/mirak/controller/ClientJoinController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-@Controller
-
-public class ClientJoinController {
-
-	private static final Logger logger = LoggerFactory.getLogger(ClientJoinController.class);
-	@RequestMapping(value = "join", method = RequestMethod.GET)
-	public void joinGET() {
-		
-		logger.info("회원가입 페이지 진입");
-	}
-}
+//@Controller
+//
+//public class ClientJoinController {
+//
+//	private static final Logger logger = LoggerFactory.getLogger(ClientJoinController.class);
+//	@RequestMapping(value = "join", method = RequestMethod.GET)
+//	public void joinGET() {
+//		
+//		logger.info("회원가입 페이지 진입");
+//	}
+//}
