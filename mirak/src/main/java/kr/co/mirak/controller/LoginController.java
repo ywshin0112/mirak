45b501dbe63896f92f36ea3b6507388854e0aac6
hi <hi@DESKTOP-6RL4QDK>
2023-03-10 16:45:00 +0900

    로그인 컨트롤러 수정중

--- mirak/src/main/java/kr/co/mirak/controller/LoginController.java
+++ mirak/src/main/java/kr/co/mirak/controller/LoginController.java
@@ -28,25 +28,25 @@ public class LoginController {
 	
 	//로그인
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ModelAndView login(MemberVO memberVO, ModelAndView mav, HttpSession session) {
+	public String login(MemberVO memberVO, HttpSession session, Model model) {
+		String returnURL = "";
 		try {
 			String mem_id = memberService.login(memberVO).getMem_id();
 			if (mem_id != null) {
 				session.setAttribute("mem_id", mem_id);
-				System.out.println("로그인 성공!!");
-				mav.setViewName("redirect:/");
+				System.out.println("로그인 성공! 메인으로 이동.");
+				returnURL = "redirect:/";
 			} else {
-				System.out.println("로그인 실패");
-				mav.setViewName("member/login");
+				System.out.println("로그인 실패ㅠ 로그인 페이지로 이동");
+				returnURL = "member/login";
 			}
-			return mav;
+			
 		}catch (Exception e) {
 			e.printStackTrace();
-			mav.setViewName("member/login");
-			mav.addObject("message", "아이디와 비밀번호 확인해주세요......");
-			System.out.println(mav);
-			return mav;
+			returnURL = "member/login";
+			model.addAttribute("message", "아이디와 비밀번호 확인해주세요......");
 		}
+		return returnURL;
 	}
 		
 	//로그아웃
