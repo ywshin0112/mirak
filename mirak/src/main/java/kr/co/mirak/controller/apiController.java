8d2bc1b576a1ef8df91087dcf4fcc14d94542e69
beeJ93 <bee93825@gmail.com>
2023-03-13 20:09:39 +0900

    구글로그인

--- mirak/src/main/java/kr/co/mirak/controller/apiController.java
+++ mirak/src/main/java/kr/co/mirak/controller/apiController.java
@@ -16,27 +16,27 @@ public class apiController {
 	@Autowired
 	private MemberService memberService;
     
-	@RequestMapping(value="/naverSave", method=RequestMethod.POST)
-	public @ResponseBody String naverSave(MemberVO vo, HttpSession session) {
-		System.out.println(vo);
-		String result = null;
-		try {
-			 if(vo != null) {
-				 int idCheck = memberService.idCheck(vo.getMem_id());
-				 if (idCheck == 0){
-					 memberService.createUser(vo);
-					 System.out.println("naverapi 회원가입 성공");
-				 }else if (idCheck == 1) {
-					 String mem_id = memberService.login(vo).getMem_id();
-					 session.setAttribute("mem_id", mem_id);
-					 System.out.println("naverapi 로그인 완료!");
-					 result="loginsuccess";
-				 }
-			 }
-				 
-		}catch(Exception e) {
-			throw new RuntimeException();
-		}
-	    return result;
-	}
+//	@RequestMapping(value="/naverSave", method=RequestMethod.POST)
+//	public @ResponseBody String naverSave(MemberVO vo, HttpSession session) {
+//		System.out.println(vo);
+//		String result = null;
+//		try {
+//			 if(vo != null) {
+//				 int idCheck = memberService.idCheck(vo.getMem_id());
+//				 if (idCheck == 0){
+//					 memberService.createUser(vo);
+//					 System.out.println("naverapi 회원가입 성공");
+//				 }else if (idCheck == 1) {
+//					 String mem_id = memberService.login(vo).getMem_id();
+//					 session.setAttribute("mem_id", mem_id);
+//					 System.out.println("naverapi 로그인 완료!");
+//					 result="loginsuccess";
+//				 }
+//			 }
+//				 
+//		}catch(Exception e) {
+//			throw new RuntimeException();
+//		}
+//	    return result;
+//	}
 }
