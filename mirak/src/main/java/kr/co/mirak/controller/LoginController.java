ab74f733525173c5d3d4db516f8752dd50e3ee2c
beeJ93 <bee93825@gmail.com>
2023-03-16 18:33:29 +0900

    마지막 머지

--- mirak/src/main/java/kr/co/mirak/controller/LoginController.java
+++ mirak/src/main/java/kr/co/mirak/controller/LoginController.java
@@ -15,23 +15,29 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.co.mirak.member.MemberService;
 import kr.co.mirak.member.MemberVO;
+import kr.co.mirak.member.login.google.GoogleOAuthConfigUtils;
 
 @Controller
 public class LoginController {
-	@Autowired
-	private MemberService memberService;
+	final static String GOOGLE_AUTH_BASE_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+	final static String GOOGLE_TOKEN_BASE_URL = "https://oauth2.googleapis.com/token";
+	final static String GOOGLE_REVOKE_TOKEN_BASE_URL = "https://oauth2.googleapis.com/revoke";
 
 	@Autowired
+	private MemberService memberService;
+	@Autowired
 	private BCryptPasswordEncoder pwEncoder;
-	
+	@Autowired
+	private GoogleOAuthConfigUtils googleUtils;
+
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-	
-	
 
 	// 로그인페이지
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginView(Model model) {
 		System.out.println("로그인 화면으로 이동...");
+		String googleUrl = googleUtils.googleInitUrl();
+		model.addAttribute( "googleUrl", googleUrl );
 		return "member/login";
 	}
 
@@ -42,35 +48,35 @@ public class LoginController {
 
 		System.out.println("login 메서드 진입");
 		System.out.println("전달된 데이터 : " + "[" + memberVO.getMem_id() + " , " + memberVO.getMem_pw()+ "]");
-		
-		
+
+
 		/* 암호화부분 */
 
 		HttpSession session = request.getSession();
-		
+
 		String rawPw = "";
 		String encodePw = "";
 
 		MemberVO lvo = memberService.login(memberVO); // 제출한아이디와 일치하는 아이디있는지
 
-		
+
 		if (lvo != null) { // 일치하는 아이디 존재시
 
 			rawPw = memberVO.getMem_pw(); // 사용자가 제출한 비번
 			encodePw = lvo.getMem_pw(); // db에 저장한 인코딩된 비번
-			
+
 			System.out.println("제출한비번:" + rawPw);
 			System.out.println("인코딩된비번:" + lvo.getMem_pw());
-			
+
 			String mem_id;
-			
+
 			if (true == pwEncoder.matches(rawPw, encodePw)) {  // 비번 일치 여부 판단
-//				logger.info("일로오나");
+				//				logger.info("일로오나");
 				memberVO.setMem_pw("");   //인코딩된 비번 정보 지움
 				mem_id = memberVO.getMem_id();
-				
-			
-				
+
+
+
 				session.setAttribute("mem_id", mem_id); //세션에 사용자정보 저장 
 				logger.info("로그인 성공");
 				return "redirect:/";  //메인페이지로 이동
@@ -82,17 +88,17 @@ public class LoginController {
 				return "member/login"; // 로그인 페이지로 이동
 			}
 		} else {   //일치하는 아이디가 존재하지 않을시 (로그인 실패)
-			
+
 			rttr.addFlashAttribute("result" , 0);
 			logger.info("로그인 실패");
 			return "member/login";  //로그인페이지로 이동
 		}
-	
+
 	}
-	
-		
-		
-/*
+
+
+
+	/*
 		System.out.println("로그인을 시도합니다.");
 		String returnURL = "";
 		String preUrl = (String) session.getAttribute("pre_url");
@@ -125,10 +131,10 @@ public class LoginController {
 		}
 		session.removeAttribute("pre_url");
 		return returnURL;
-		
+
 	}
 
-*/
+	 */
 
 	// 로그아웃
 	@RequestMapping("/logout")
@@ -138,26 +144,26 @@ public class LoginController {
 		System.out.println("로그아웃 성공!!");
 		return "redirect:/";
 	}
-	
+
 	// 아이디찾기 
 	@RequestMapping(value = "/idfind", method = RequestMethod.GET)
 	public String idfindform() {
 		System.out.println("아이디 찾기 이동..");
 		return "member/idfind";
 	}
-	
+
 	// 아이디 찾기
 	@RequestMapping(value = "/idfind", method = RequestMethod.POST)
 	public String idfind(MemberVO vo, HttpSession session, Model model) {
 		String returnURL = "member/idfind";
 		String preUrl = (String) session.getAttribute("pre_url");
 		System.out.println("아이디찾기중... , preUrl : " + preUrl);
-		
+
 		try {
-		MemberVO member = memberService.idfind(vo);
-		model.addAttribute("member", member);
-		System.out.println(member);
-		
+			MemberVO member = memberService.idfind(vo);
+			model.addAttribute("member", member);
+			System.out.println(member);
+
 			if (member.getMem_id() != null) {
 				model.addAttribute("mem_id", member.getMem_id());
 				System.out.println(member.getMem_id());
@@ -175,36 +181,35 @@ public class LoginController {
 		}
 		return returnURL;
 	}	
-	
-	// 비번재설정
-		@RequestMapping(value = "/pwreset", method = RequestMethod.GET)
-		public String pwresetform() {
-			System.out.println("비번 재설정 이동..");
-			return "member/pwreset";
-		}
 
-		// 비번재설정
-		@RequestMapping(value = "/pwreset", method = RequestMethod.POST)
-		public String pwreset(MemberVO vo, Model model, RedirectAttributes rttr) {
-			System.out.println("비밀번호 재설정중....");
-			System.out.println(vo);
-			
-			try {
-				if(vo.getMem_id() == null) {
-					memberService.idfind_pw(vo).getMem_id();
-				}
-				if (vo.getMem_id() != null && vo.getMem_pw() != null) {
-					memberService.pwreset(vo);
-					rttr.addFlashAttribute("message","비밀번호가 변경되었습니다.");
-					return "redirect:/";
-				}
-				model.addAttribute("mem_id", vo.getMem_id());
-				return "member/pwreset";
-			} catch (Exception e) {
-				e.printStackTrace();
-				model.addAttribute("message", "정보를 다시 입력해주세요....");
+	// 비번재설정
+	@RequestMapping(value = "/pwreset", method = RequestMethod.GET)
+	public String pwresetform() {
+		System.out.println("비번 재설정 이동..");
+		return "member/pwreset";
+	}
+
+	// 비번재설정
+	@RequestMapping(value = "/pwreset", method = RequestMethod.POST)
+	public String pwreset(MemberVO vo, Model model, RedirectAttributes rttr) {
+		System.out.println("비밀번호 재설정중....");
+		System.out.println(vo);
+
+		try {
+			if(vo.getMem_id() == null) {
+				memberService.idfind_pw(vo).getMem_id();
 			}
+			if (vo.getMem_id() != null && vo.getMem_pw() != null) {
+				memberService.pwreset(vo);
+				rttr.addFlashAttribute("message","비밀번호가 변경되었습니다.");
+				return "redirect:/";
+			}
+			model.addAttribute("mem_id", vo.getMem_id());
 			return "member/pwreset";
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("message", "정보를 다시 입력해주세요....");
 		}
-		
+		return "member/pwreset";
+	}
 }
--- mirak/src/main/java/kr/co/mirak/controller/apiController.java
+++ mirak/src/main/java/kr/co/mirak/controller/apiController.java
@@ -1,51 +1,52 @@
 package kr.co.mirak.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.PrintWriter;
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.co.mirak.member.MemberService;
 import kr.co.mirak.member.MemberVO;
+import kr.co.mirak.member.login.google.SnsLoginService;
 
 @Controller
 public class apiController {
 	@Autowired
 	private MemberService memberService;
-    
+	@Autowired
+	private SnsLoginService snsLoginService;
+
 	@RequestMapping(value="/naverSave", method=RequestMethod.POST)
 	@ResponseBody
 	public String naverSave(MemberVO vo, HttpSession session) {
 		System.out.println(vo);
 		String result = null;
 		try {
-			 if(vo != null) {
-				 int idCheck = memberService.idCheck(vo.getMem_id());
-				 if (idCheck == 0){
-					 memberService.createUser(vo);
-					 session.setAttribute("mem_id", vo.getMem_id());
-					 session.setAttribute("message","회원가입 성공하였습니다.");
-					 System.out.println("naverapi 회원가입 성공");
-				 }else if (idCheck == 1) {
-					 String mem_id = memberService.login(vo).getMem_id();
-					 session.setAttribute("mem_id", mem_id);
-					 session.setAttribute("message","로그인에 성공하였습니다.");
-					 System.out.println("naverapi 로그인 완료!");
-					 result="loginsuccess";
-				 }
-			 }
+			if(vo != null) {
+				int idCheck = memberService.idCheck(vo.getMem_id());
+				if (idCheck == 0){
+					memberService.createUser(vo);
+					session.setAttribute("mem_id", vo.getMem_id());
+					session.setAttribute("message","회원가입 성공하였습니다.");
+					System.out.println("naverapi 회원가입 성공");
+				}else if (idCheck == 1) {
+					String mem_id = memberService.login(vo).getMem_id();
+					session.setAttribute("mem_id", mem_id);
+					session.setAttribute("message","로그인에 성공하였습니다.");
+					System.out.println("naverapi 로그인 완료!");
+					result="loginsuccess";
+				}
+			}
 		} catch (Exception e) {
 			throw new RuntimeException();
 		}
@@ -58,17 +59,41 @@ public class apiController {
 	public String kakaoLogin(@RequestParam(value = "code", required = false) String code) throws Exception {
 		System.out.println("#########" + code);
 		String access_Token = memberService.getAccessToken(code);
-		
-		
+
+
 		// 위에서 만든 코드 아래에 코드 추가
 		HashMap<String, Object> userInfo = memberService.getUserInfo(access_Token);
 		System.out.println("###access_Token#### : " + access_Token);
 		System.out.println("###nickname#### : " + userInfo.get("nickname"));
 		System.out.println("###email#### : " + userInfo.get("email"));
-		
-		
+
+
 		return "member/join";
 	}
-	
+
+
+
+	/**
+	 * 구글 로그인~!
+	 * Authentication Code를 전달 받는 엔드포인트
+	 **/
+	@RequestMapping(value="/login/google/auth", method=RequestMethod.GET)
+	public String googleAuth(Model model, @RequestParam(value = "code", required = false) String code, HttpServletResponse response) throws IOException {
+		//Google OAuth Access Token 요청을 위한 파라미터 세팅
+		System.out.println("authorize_code : " + code);
+		String access_Token = snsLoginService.getGoogleAccessToken(code);
+
+		HashMap<String, Object> googleUserInfo = snsLoginService.getGoogleUserInfo(access_Token);
+		System.out.println("###access_Token#### : " + access_Token);
+		System.out.println("###nickname#### : " + googleUserInfo.get("nickname"));
+		System.out.println("###email#### : " + googleUserInfo.get("email"));
+
+		PrintWriter out = response.getWriter();
+		out.println("<script>window.close(); opener.parent.location="+"'/join'"+";</script>");
+		out.flush();
+
+		return "member/join";
+	}	
+
 
 }
