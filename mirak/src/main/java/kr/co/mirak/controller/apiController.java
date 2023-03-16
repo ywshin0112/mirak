44bcda1314f4ec12d494fec29834fdc3ad52ddaa
Lanjangju <acj119@naver.com>
2023-03-16 17:23:50 +0900

    카카오로그인

--- mirak/src/main/java/kr/co/mirak/controller/apiController.java
+++ mirak/src/main/java/kr/co/mirak/controller/apiController.java
@@ -1,11 +1,19 @@
 package kr.co.mirak.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.mirak.member.MemberService;
@@ -15,29 +23,49 @@ import kr.co.mirak.member.MemberVO;
 public class apiController {
 	@Autowired
 	private MemberService memberService;
-    
-	@RequestMapping(value="/naverSave", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/naverSave", method = RequestMethod.POST)
 //	@ResponseBody // 네이버 강제로그인 되서 일단 주석함 ㅠㅠ... 페이지 이동 안되요.
 	public @ResponseBody String naverSave(MemberVO vo, HttpSession session) {
 		System.out.println(vo);
 		String result = null;
 		try {
-			 if(vo != null) {
-				 int idCheck = memberService.idCheck(vo.getMem_id());
-				 if (idCheck == 0){
-					 memberService.createUser(vo);
-					 System.out.println("naverapi 회원가입 성공");
-				 }else if (idCheck == 1) {
-					 String mem_id = memberService.login(vo).getMem_id();
-					 session.setAttribute("mem_id", mem_id);
-					 System.out.println("naverapi 로그인 완료!");
-					 result="loginsuccess";
-				 }
-			 }
-				 
-		}catch(Exception e) {
+			if (vo != null) {
+				int idCheck = memberService.idCheck(vo.getMem_id());
+				if (idCheck == 0) {
+					memberService.createUser(vo);
+					System.out.println("naverapi 회원가입 성공");
+				} else if (idCheck == 1) {
+					String mem_id = memberService.login(vo).getMem_id();
+					session.setAttribute("mem_id", mem_id);
+					System.out.println("naverapi 로그인 완료!");
+					result = "loginsuccess";
+				}
+			}
+
+		} catch (Exception e) {
 			throw new RuntimeException();
 		}
-	    return result;
+		return result;
 	}
+
+	// 카카오 로그인
+	@RequestMapping(value = "/kakaoLogin")
+
+	public String kakaoLogin(@RequestParam(value = "code", required = false) String code) throws Exception {
+		System.out.println("#########" + code);
+		String access_Token = memberService.getAccessToken(code);
+		
+		
+		// 위에서 만든 코드 아래에 코드 추가
+		HashMap<String, Object> userInfo = memberService.getUserInfo(access_Token);
+		System.out.println("###access_Token#### : " + access_Token);
+		System.out.println("###nickname#### : " + userInfo.get("nickname"));
+		System.out.println("###email#### : " + userInfo.get("email"));
+		
+		
+		return "member/join";
+	}
+	
+
 }
--- mirak/src/main/java/kr/co/mirak/member/MemberServiceImpl.java
+++ mirak/src/main/java/kr/co/mirak/member/MemberServiceImpl.java
@@ -1,91 +1,238 @@
 
 package kr.co.mirak.member;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
 import org.mybatis.spring.SqlSessionTemplate;
 
-import kr.co.mirak.product.Criteria;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class MemberServiceImpl implements MemberService {
 
 	private SqlSessionTemplate sqlSessionTemplate;
-	
-	public MemberServiceImpl() {}
-	
+
+	public MemberServiceImpl() {
+	}
+
 	public MemberServiceImpl(SqlSessionTemplate sqlSessionTemplate) {
 		this.sqlSessionTemplate = sqlSessionTemplate;
 	}
-	
-	//로그인
+
+	// 로그인
 	public MemberVO login(MemberVO vo) {
 		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		MemberVO memVO  = mapper.login(vo);
+		MemberVO memVO = mapper.login(vo);
 		return memVO;
 	}
-	
-	//아이디 찾기
+
+	// 아이디 찾기
 	public MemberVO idfind(MemberVO vo) {
 		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		MemberVO memVO  = mapper.idfind(vo);
+		MemberVO memVO = mapper.idfind(vo);
 		return memVO;
 	}
-	
-	//아이디 찾기(비번 리셋용)
+
+	// 아이디 찾기(비번 리셋용)
 	public MemberVO idfind_pw(MemberVO vo) {
 		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		MemberVO memVO  = mapper.idfind(vo);
+		MemberVO memVO = mapper.idfind(vo);
 		return memVO;
 	}
-	
-	//회원가입
+
+	// 회원가입
 	@Override
 	public int createUser(MemberVO vo) {
 		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		int success  = mapper.createUser(vo);
+		int success = mapper.createUser(vo);
 		return success;
 	}
+
 	@Override
 	public int naverinsert(MemberVO vo) {
 		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		int success  = mapper.naverinsert(vo);
+		int success = mapper.naverinsert(vo);
 		return success;
 	}
-	//세션 값으로 회원 정보 확인
-	public MemberVO getMemberInfo(HttpSession session){
-		String myid = (String)session.getAttribute("mem_id");
+
+	// 세션 값으로 회원 정보 확인
+	public MemberVO getMemberInfo(HttpSession session) {
+		String myid = (String) session.getAttribute("mem_id");
 		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		MemberVO memVO  = mapper.getMemberInfo(myid);
+		MemberVO memVO = mapper.getMemberInfo(myid);
 		return memVO;
 	}
-	
+
 	public int memupdate(MemberVO vo) {
 		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		int updatesuccess  = mapper.memupdate(vo);
+		int updatesuccess = mapper.memupdate(vo);
 		return updatesuccess;
 	}
 
 	@Override
-	public int memdelete(MemberVO vo){
+	public int memdelete(MemberVO vo) {
 		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
 		int seccess = mapper.memdelete(vo);
-		return seccess;	
+		return seccess;
 	}
-	
 
-	//아이디체크
+	// 아이디체크
 	@Override
-	public int idCheck(String id) throws Exception{
+	public int idCheck(String id) throws Exception {
 		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
 		int result = mapper.idCheck(id);
 		return result;
 	}
 
+	// kakao
 	
-	//ADMIN 리스트
+	
+	@Override
+	public String getAccessToken(String code) {
+		String access_Token = "";
+		String refresh_Token = "";
+		String reqURL = "https://kauth.kakao.com/oauth/token";
+
+		try {
+			URL url = new URL(reqURL);
+
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			// POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			// POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+			StringBuilder sb = new StringBuilder();
+			sb.append("grant_type=authorization_code");
+
+			sb.append("&client_id=e481c91b1136f51f927a619fc062146d"); // 본인이 발급받은 key
+			sb.append("&redirect_uri=http://localhost:8080/kakaoLogin"); // 본인이 설정한 주소
+
+			sb.append("&code=" + code);
+			bw.write(sb.toString());
+			bw.flush();
+
+			// 결과 코드가 200이라면 성공
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+
+			// 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			String result = "";
+
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body : " + result);
+
+			// Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+			JsonParser parser = new JsonParser();
+			JsonElement element = parser.parse(result);
+
+			access_Token = element.getAsJsonObject().get("access_token").getAsString();
+			refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
+
+			System.out.println("access_token : " + access_Token);
+			System.out.println("refresh_token : " + refresh_Token);
+
+			br.close();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return access_Token;
+	}
+
+	@Override
+	public HashMap<String, Object> getUserInfo(String access_Token) {
+		// 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+		HashMap<String, Object> userInfo = new HashMap<String, Object>();
+		String reqURL = "https://kapi.kakao.com/v2/user/me";
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+
+			// 요청에 필요한 Header에 포함될 내용
+			conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+			String line = "";
+			String result = "";
+
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body : " + result);
+
+			JsonParser parser = new JsonParser();
+			JsonElement element = parser.parse(result);
+
+			JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+			JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+			String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+			String email = kakao_account.getAsJsonObject().get("email").getAsString();
+
+			userInfo.put("nickname", nickname);
+			userInfo.put("email", email);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return userInfo;
+	}
+
+	
+	
+	
+	// ADMIN ID값으로 회원 정보 확인
+	public MemberVO getMemberDetail(String memId) {
+		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
+		MemberVO memVO = mapper.getMemberInfo(memId);
+		return memVO;
+	}
+
+	// 김원중이 건드린 부분
+
+	public int getTotal() {
+		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
+		return mapper.getTotal();
+	}
+
+	public List<MemberVO> getListPaging(CriteriaM cri) {
+		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
+		return mapper.getListPaging(cri);
+	}
+
+	public MemberVO getMemberDetail(MemberVO mvo) {
+
+		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
+		return mapper.getMemberDetail(mvo);
+
+	}
+
+	// 여기까지
+
+	// ADMIN 리스트
 //	public List<MemberVO> getMemberList(Optional<Integer> pageStart) {
 //		int page;
 //		if (pageStart.isPresent()) {
@@ -99,39 +246,5 @@ public class MemberServiceImpl implements MemberService {
 //		System.out.println(page+"페이지로 이동");
 //		return memberList;
 //	}
-	
-	
-	
-	//ADMIN ID값으로 회원 정보 확인
-	public MemberVO getMemberDetail(String memId){
-		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		MemberVO memVO  = mapper.getMemberInfo(memId);
-		return memVO;
-	}
-	
-	//김원중이 건드린 부분
-	
-	public int getTotal() {
-		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		return mapper.getTotal();
-	}
-	
-	public List<MemberVO> getListPaging(CriteriaM cri) {
-		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		return mapper.getListPaging(cri);
-	}
-	
-	public MemberVO getMemberDetail(MemberVO mvo) {
-		
-		MemberMapper mapper = sqlSessionTemplate.getMapper(MemberMapper.class);
-		return mapper.getMemberDetail(mvo);
-		
-	}
-	
-	
-
-	//여기까지
-	
-	
 
 }
