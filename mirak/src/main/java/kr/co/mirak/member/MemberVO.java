f1af895ba7b183871f65a12b8d8d207a83ba8aec
beeJ93 <bee93825@gmail.com>
2023-03-17 18:00:24 +0900

    구글API 로그인, 회원가입

--- mirak/src/main/java/kr/co/mirak/member/MemberVO.java
+++ mirak/src/main/java/kr/co/mirak/member/MemberVO.java
@@ -3,10 +3,10 @@ package kr.co.mirak.member;
 import java.util.Date;
 
 public class MemberVO {
-	private String mem_id, mem_pw, mem_name, mem_phone, mem_zipcode, mem_add1, mem_add2, mem_token, mem_reset;
+	private String mem_id, mem_pw, mem_name, mem_phone, mem_zipcode, mem_add1, mem_add2;
 	private int mem_age, mem_gender;
 	private Date mem_regdate;
-	
+	private Boolean mem_isapi;
 	public String getMem_id() {
 		return mem_id;
 	}
@@ -49,18 +49,6 @@ public class MemberVO {
 	public void setMem_add2(String mem_add2) {
 		this.mem_add2 = mem_add2;
 	}
-	public String getMem_token() {
-		return mem_token;
-	}
-	public void setMem_token(String mem_token) {
-		this.mem_token = mem_token;
-	}
-	public String getMem_reset() {
-		return mem_reset;
-	}
-	public void setMem_reset(String mem_reset) {
-		this.mem_reset = mem_reset;
-	}
 	public int getMem_age() {
 		return mem_age;
 	}
@@ -79,11 +67,22 @@ public class MemberVO {
 	public void setMem_regdate(Date mem_regdate) {
 		this.mem_regdate = mem_regdate;
 	}
+	public Boolean getMem_isapi() {
+		return mem_isapi;
+	}
+	public void setMem_isapi(Boolean mem_isapi) {
+		this.mem_isapi = mem_isapi;
+	}
 	@Override
 	public String toString() {
 		return "MemberVO [mem_id=" + mem_id + ", mem_pw=" + mem_pw + ", mem_name=" + mem_name + ", mem_phone="
 				+ mem_phone + ", mem_zipcode=" + mem_zipcode + ", mem_add1=" + mem_add1 + ", mem_add2=" + mem_add2
-				+ ", mem_token=" + mem_token + ", mem_reset=" + mem_reset + ", mem_age=" + mem_age + ", mem_gender="
-				+ mem_gender + ", mem_regdate=" + mem_regdate + "]";
+				+ ", mem_age=" + mem_age + ", mem_gender=" + mem_gender + ", mem_regdate=" + mem_regdate
+				+ ", mem_isapi=" + mem_isapi + ", getMem_id()=" + getMem_id() + ", getMem_pw()=" + getMem_pw()
+				+ ", getMem_name()=" + getMem_name() + ", getMem_phone()=" + getMem_phone() + ", getMem_zipcode()="
+				+ getMem_zipcode() + ", getMem_add1()=" + getMem_add1() + ", getMem_add2()=" + getMem_add2()
+				+ ", getMem_age()=" + getMem_age() + ", getMem_gender()=" + getMem_gender() + ", getMem_regdate()="
+				+ getMem_regdate() + ", getMem_isapi()=" + getMem_isapi() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
 	}
 }
