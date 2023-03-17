8d28adb67a5cfed62a8633e1afe220495269d2e0
beeJ93 <bee93825@gmail.com>
2023-03-17 11:00:28 +0900

    어드민 비밀번호 기능 추가

--- mirak/src/main/java/kr/co/mirak/controller/AdminLoginController.java
+++ mirak/src/main/java/kr/co/mirak/controller/AdminLoginController.java
@@ -3,6 +3,7 @@ package kr.co.mirak.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,9 @@ import kr.co.mirak.member.MemberVO;
 public class AdminLoginController {	
 	@Autowired
 	private MemberService memberService;
+
+	@Autowired
+	private BCryptPasswordEncoder pwEncoder;
 	
 	@RequestMapping(value={"/admin"}, method=RequestMethod.GET)
 	public String adminLoginGet() {
@@ -23,25 +27,32 @@ public class AdminLoginController {
 	
 	@RequestMapping(value={"/admin"}, method=RequestMethod.POST)
 	public String adminLoginPost(MemberVO memberVO, HttpSession session) {
+		System.out.println("=== 어드민 로그인 시도중 ===");
+		String rawPw = memberVO.getMem_pw(); // 사용자가 제출한 비번
+		String encodePw = ""; 
+		MemberVO lvo = memberService.login(memberVO);
 		try {
-			memberService.login(memberVO);
-			String mem_id = memberVO.getMem_id();
-			if (mem_id != null) {
-				session.setAttribute("mem_id", mem_id);
-				if (mem_id.equals("admin")) {
+			if (lvo != null) { 
+				encodePw = lvo.getMem_pw(); 
+				memberService.login(memberVO);
+				System.out.println("제출한비번 :" + rawPw);		
+				System.out.println("인코딩된비번 :" + encodePw);		
+				String mem_id = memberVO.getMem_id();
+				if (true == pwEncoder.matches(rawPw, encodePw) && mem_id.equals("admin")) {
+					session.setAttribute("mem_id", mem_id);
 					System.out.println("관리자 로그인 성공. 차트페이지로 이동.");
-					return "redirect:/admin/members";
-				}else{
-					System.out.println("관리자가 아닙니다.");
-					return "redirect:/";
+					return "redirect:/admin/charts";
+					
 				}
+				System.out.println("관리자가 아닙니다.");
+				return "redirect:/";
 			} else {
-				System.out.println("로그인 실패1");
+				System.out.println("로그인 실패. 존재하지 않는 아이디입니다.");
 				return "redirect:/admin";
 			}
 		}catch (Exception e) {
 			e.printStackTrace();
-			System.out.println("로그인 실패2");
+			System.out.println("로그인 실패.");
 			return "redirect:/admin";
 		}
 	}
