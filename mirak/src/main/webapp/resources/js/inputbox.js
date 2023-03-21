fa6bfaace34c4d68bbc75fe263ab55f419a78bcf
Lanjangju <119823603+Lanjangju@users.noreply.github.com>
2023-03-21 16:58:30 +0900

    Add files via upload
    주소 경고창 나이 경고창

---
+++ mirak/src/main/webapp/resources/js/inputbox.js
@@ -0,0 +1,21 @@
+function checkAddressInput() {
+    var zipcode = $("#address_input_1").val();
+    if (zipcode == "") {
+        alert("우편번호를 입력해주세요.");
+        return false;
+    }
+    return true;
+}
+
+
+function checkAgeInput() {
+    var ageInput = document.getElementById("age");
+    var age = ageInput.value;
+    if (age <= 0) {
+        alert("나이를 입력해주세요");
+        ageInput.focus();
+        return false;
+    }
+    return true;
+}
+
