7c08887bb4422fba1f613484865557e10a28df0c
Lanjangju <acj119@naver.com>
2023-03-30 17:05:04 +0900

    검색기능 재수정중

--- mirak/src/main/java/kr/co/mirak/cart/CartMapper.java
+++ mirak/src/main/java/kr/co/mirak/cart/CartMapper.java
@@ -26,6 +26,8 @@ public interface CartMapper {
 	/* 게시판 총 갯수 */
     public int getTotal();
     
+//    public int getTotal(CriteriaC cri);
+    
     /* 게시판 목록(페이징 적용) */
     public List<CartVO> getListPaging(CriteriaC cri);	
 	
