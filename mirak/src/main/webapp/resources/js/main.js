0bf41516e5331c14d23ff4cf9d906cb41dea5d99
beeJ93 <bee93825@gmail.com>
2023-03-31 15:36:00 +0900

    어드민 결재 디자인 변경, 클라이언트 하단 패딩 수정

--- mirak/src/main/webapp/resources/js/main.js
+++ mirak/src/main/webapp/resources/js/main.js
@@ -142,34 +142,7 @@
 					navbar = $('.ftco_navbar'),
 					sd = $('.js-scroll-wrap');
 
-			if (st > 150) {
-				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
-				}
-			} 
-			if (st < 150) {
-				if ( navbar.hasClass('scrolled') ) {
-					navbar.removeClass('scrolled sleep');
-				}
-			} 
-			if ( st > 350 ) {
-				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
-				}
-				
-				if(sd.length > 0) {
-					sd.addClass('sleep');
-				}
-			}
-			if ( st < 350 ) {
-				if ( navbar.hasClass('awake') ) {
-					navbar.removeClass('awake');
-					navbar.addClass('sleep');
-				}
-				if(sd.length > 0) {
-					sd.removeClass('sleep');
-				}
-			}
+			
 		});
 	};
 	scrollWindow();
