13afa29657e203c08efc33d44349f29143c6d207
beeJ93 <bee93825@gmail.com>
2023-04-04 12:00:49 +0900

    어드민 로그인되게 수정~

--- mirak/src/main/java/kr/co/mirak/controller/AdminLoginController.java
+++ mirak/src/main/java/kr/co/mirak/controller/AdminLoginController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,13 +27,15 @@ public class AdminLoginController {
 	}
 	
 	@RequestMapping(value={"/admin"}, method=RequestMethod.POST)
-	public String adminLoginPost(MemberVO memberVO, HttpSession session) {
+	public String adminLoginPost(MemberVO memberVO, HttpSession session, Model model) {
 		System.out.println("=== 어드민 로그인 시도중 ===");
 		String rawPw = memberVO.getMem_pw(); // 사용자가 제출한 비번
 		String encodePw = ""; 
 		memberVO.setMem_isapi("normal");
 		System.out.println(memberVO);
 		MemberVO lvo = memberService.login(memberVO);
+		String url = "member/admin_login";
+		String result = "";
 		try {
 			if (lvo != null) { 
 				encodePw = lvo.getMem_pw(); 
@@ -43,27 +46,30 @@ public class AdminLoginController {
 				if (true == pwEncoder.matches(rawPw, encodePw) && mem_id.equals("admin")) {
 					session.setAttribute("mem_id", mem_id);
 					System.out.println("관리자 로그인 성공. 차트페이지로 이동.");
-					return "redirect:/admin/charts";
+					url = "redirect:/admin/charts";
 					
+				}else {
+					result = "관리자가 아닙니다.";
+					System.out.println("관리자가 아닙니다.");
 				}
-				System.out.println("관리자가 아닙니다.");
-				return "redirect:/admin";
 			} else {
+				result = "관리자가 아닙니다.";
 				System.out.println("로그인 실패. 존재하지 않는 아이디입니다.");
-				return "redirect:/admin";
 			}
 		}catch (Exception e) {
 			e.printStackTrace();
+			result = "로그인에 실패하였습니다.";
 			System.out.println("로그인 실패.");
-			return "redirect:/admin";
 		}
+		model.addAttribute("result", result);
+		return url;
 	}
 	
 	// 로그아웃
 	@RequestMapping("/admin/logout")
 	public String logout(HttpSession session) {
 		session.invalidate();
-		System.out.println("로그아웃 성공!!");
+		System.out.println("어드민 로그아웃 성공!!");
 		return "redirect:/admin";
 	}
 }
